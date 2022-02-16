508e9cd5891fc5e2a807634c48969e06f75ad983
Pradeep Selastian <pradeepselastian@LIVE.COM>
2022-02-16 12:39:46 +0000

    Push1

---
+++ Bosch_CRMTraining/MultiLevel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bosch_CRMTraining
+{
+    public interface IRectangle
+    {
+        void AreaOfRectangle();
+    }
+
+    public class AreaRectAndCircle1
+    {
+        public int length = 0;
+        public int breadth = 0;
+        public decimal radius = 0.0M;
+
+
+    }
+    public class Child1 : AreaRectAndCircle1
+    {
+        public void CalculateAreaOfRectangle()
+        {
+            Console.WriteLine("Area of Rectangle:{0}", (length * breadth));
+        }
+
+    }
+
+    //public class child2: Child1
+    //{
+
+    //    protected void CalculateAreaOfCircle()
+    //    {
+    //        Console.WriteLine("Area of Circle: {0}", ((3.14M) * (radius * radius)));
+
+    //    }
+
+    //}
+
+    class MultiLevelExample
+    {
+        public static void Main()
+        {
+            Child1 c2 = new Child1();
+            c2.length = 10;
+            c2.breadth = 20;
+            c2.radius = 2.0M;
+            c2.CalculateAreaOfRectangle();
+            //c2.CalculateAreaOfCircle();
+            Console.ReadLine();
+
+        }
+
+    }
+}
--- Bosch_CRMTraining/Program.cs
+++ Bosch_CRMTraining/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Bosch_CRMTraining
 {
+    static class AreaRectAndCircle
+    {
+        public static decimal CalculateAreaOfCircle(decimal radius)
+        {
+            return ((3.14M) * (radius * radius));
+        }
+
+        public static int CalculateAreaOfRectangle(int length, int breadth)
+        {
+            return (length * breadth);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1. Enter 1 to calclate Area of Rectangle." +
+                "\n2. Enter 2 to calculate Area of Cicle.\nPlease enter your choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1 || choice == 2)
+            {
+                if (choice == 1)
+                {
+                    Console.WriteLine("Enter Lenght of Rectanlge:");
+                    int length = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Breadth of Rectanlge:");
+                    int breadth = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Area of Rectangle = {0}", AreaRectAndCircle.CalculateAreaOfRectangle(length, breadth));
+                }
+                else
+                {
+                    Console.WriteLine("Enter radius of circle:");
+                    decimal radius = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine("Area of Circle = {0}", AreaRectAndCircle.CalculateAreaOfCircle(radius));
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have entered and invalid choce..");
+            }
+            Console.ReadLine();
         }
     }
 }
