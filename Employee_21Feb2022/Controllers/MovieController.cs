ae728c269b64f95b5da41c72441ecdfd6f07e3f5
Pradeep Selastian <pradeepselastian@LIVE.COM>
2022-02-22 09:37:00 +0000

    22Feb2021-Session Tasks

--- Employee_21Feb2022/Controllers/MovieController.cs
+++ Employee_21Feb2022/Controllers/MovieController.cs
@@ -9,17 +9,70 @@ namespace Employee_21Feb2022.Controllers
 {
     public class MovieController : Controller
     {
-        // GET: Movie
-        public ActionResult GetMoviewDetails()
+        List<MovieModel> lstMovieModel = null;
+        public MovieController()
         {
-            MovieModel mvMd = new MovieModel();
-            mvMd.Title = "RRR";
-            mvMd.ReleaseDate = "21 Dec 2021";
-            mvMd.Rating = 4.5M;
-            mvMd.Genre = "Action";
-            mvMd.Price = 5000000;
-
-            return View(mvMd);
+            lstMovieModel = new List<MovieModel>()
+            {
+                new MovieModel{ ID= 1, Title="RRR", Genre="Action", ReleaseDate="20 Jan 2022", Rating=5.0M, Price=1000},
+                new MovieModel{ ID= 2, Title="KGF", Genre="Action", ReleaseDate="21 Jan 2022", Rating=5.0M, Price=2000},
+                new MovieModel{ ID= 3, Title="Bahubali-2", Genre="Action", ReleaseDate="26 Jan 2022", Rating=5.0M, Price=3000},
+                new MovieModel{ ID= 4, Title="Anubavinchu Raja", Genre="Comedy", ReleaseDate="30 Jan 2022", Rating=5.0M, Price=800}
+            };
         }
+
+        // GET: Movie
+        public ActionResult GetMoviewDetails(List<MovieModel> lstMovieModel1)
+        {
+            //MovieModel mvMd = new MovieModel();
+            //mvMd.Title = "RRR";
+            //mvMd.ReleaseDate = "21 Dec 2021";
+            //mvMd.Rating = 4.5M;
+            //mvMd.Genre = "Action";
+            //mvMd.Price = 5000000;
+            if (lstMovieModel1 == null)
+            {
+                lstMovieModel1 = lstMovieModel;
+            }
+
+            return View(lstMovieModel1);
+        }
+
+        [HttpGet]
+        public ActionResult ModifyMovie(int ID)
+        {
+            MovieModel mv = null;
+            var movieFound = lstMovieModel.Where(x => x.ID == ID).FirstOrDefault();
+            if (movieFound != null)
+            {
+                mv = movieFound;
+            }
+
+            return View(mv);
+        }
+
+        [HttpPost]
+        public ActionResult ModifyMovie(MovieModel mv)
+        {
+            MovieModel mv1 = null;
+            var movieFound = lstMovieModel.Where(x => x.ID == mv.ID).FirstOrDefault();
+            if (movieFound != null)
+            {
+                movieFound.Title = mv.Title;
+                movieFound.Genre = mv.Genre;
+                movieFound.ReleaseDate = mv.ReleaseDate;
+                movieFound.Rating = mv.Rating;
+                movieFound.Price = mv.Price;
+            }
+
+            return View("GetMoviewDetails", lstMovieModel);
+            //return RedirectToAction("MovieDetail", mv);
+        }
+
+        public ActionResult MovieDetail(MovieModel mv)
+        {
+            return View(mv);
+        }
+
     }
 }
--- Employee_21Feb2022/Models/MovieModel.cs
+++ Employee_21Feb2022/Models/MovieModel.cs
@@ -8,6 +8,7 @@ namespace Employee_21Feb2022.Models
     public class MovieModel
     {
         //title,releasing date, genre, rating, price
+        public int ID { get; set; }
         public string Title { get; set; }
         public string ReleaseDate { get; set; }
         public string Genre { get; set; }
